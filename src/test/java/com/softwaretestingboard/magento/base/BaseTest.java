f7a69ac67ad9d3762cfb08be5a87467356febe69
Hassan-Abdelaziz <hassan.abdelaziz155@gmail.com>
2024-10-14 07:31:09 +0300

    Add ad Block extensions

--- src/test/java/com/softwaretestingboard/magento/base/BaseTest.java
+++ src/test/java/com/softwaretestingboard/magento/base/BaseTest.java
@@ -2,9 +2,14 @@ package com.softwaretestingboard.magento.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseTest {
 
@@ -12,14 +17,28 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup(){
-        driver = new EdgeDriver();
+        //  Add ad block extension
+        EdgeOptions options = new EdgeOptions();
+        options.addExtensions (new File("src/test/resources/Plus Ad Blocker - Chrome Web Store 0.0.1.0.crx"));
+
+        driver = new EdgeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://magento.softwaretestingboard.com/");
+
     }
 
     @AfterMethod
     public void teardown(){
         driver.quit();
     }
+
+
+
+//    JavascriptExecutor js;
+//if (driver instanceof JavascriptExecutor) {
+//        js = (JavascriptExecutor) driver;
+//    }
+//js.executeScript("return document.getElementsByClassName('review-info-star')[0].remove();");
+
 }
